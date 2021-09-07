6fccbb1edac6961c36f094cae8b91d0738c3c55b
Benjamin Marius Lank <benj007d@elevcampus.dk>
2021-09-07 06:29:10 +0200

    Finished creating the game board.

--- Snake-WPF/MainWindow.xaml.cs
+++ Snake-WPF/MainWindow.xaml.cs
@@ -20,12 +20,59 @@ namespace Snake_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
-
-
+        //Snake body parts.
+        const int SnakeSquareSize = 20;
 
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Called when launching application.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+            DrawGameArea();
+        }
+
+        /// <summary>
+        /// Drawing the gameboard.
+        /// </summary>
+        private void DrawGameArea()
+        {
+            bool doneDrawingBackground = false;
+            int nextX = 0, nextY = 0;
+            int rowCounter = 0;
+            bool nextIsOdd = false;
+
+            while (doneDrawingBackground == false)
+            {
+                Rectangle rect = new Rectangle
+                {
+                    Width = SnakeSquareSize,
+                    Height = SnakeSquareSize,
+                    Fill = nextIsOdd ? Brushes.White : Brushes.Black
+                };
+                GameArea.Children.Add(rect);
+                Canvas.SetTop(rect, nextY);
+                Canvas.SetLeft(rect, nextX);
+
+                nextIsOdd = !nextIsOdd;
+                nextX += SnakeSquareSize;
+                if (nextX >= GameArea.ActualWidth)
+                {
+                    nextX = 0;
+                    nextY += SnakeSquareSize;
+                    rowCounter++;
+                    nextIsOdd = (rowCounter % 2 != 0);
+                }
+
+                if (nextY >= GameArea.ActualHeight)
+                    doneDrawingBackground = true;
+            }
         }
     }
 }
---
+++ Snake-WPF/SnakePart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace Snake_WPF
+{
+    public class SnakePart
+    {
+        public UIElement UiElement { get; set; }
+
+        public Point Position { get; set; }
+
+        public bool IsHead { get; set; }
+    }
+}
